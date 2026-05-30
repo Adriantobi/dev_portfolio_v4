8b84120176e241bf3885ab414b1406cb5353c8b0
Adrian Tobi <talktotobi.a@gmail.com>
2026-05-30 20:30:12 +0100

    insights and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +32,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <div className="background h-full w-full pointer-events-none fixed inset-0"></div>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
